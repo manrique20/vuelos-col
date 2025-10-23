abc98317eb1add345828366861ef26baf5efd00a
Manrique2026 <smanrique@kubo.co>
2025-10-22 19:45:43 -0500

    [structure]

--- stores/Main.store.ts
+++ stores/Main.store.ts
@@ -1,24 +1,7 @@
 import { defineStore } from "pinia";
 
-interface Coordinates {
-  latitude?: string;
-  longitude?: string;
-  accuracy?: string;
-  speed?: string | null;
-  heading?: string | null;
-  timestamp: string
-}
 export const useMainStore = defineStore("main", {
   state: () => ({
     loading: false,
-    check: false,
-    showDialog: false,
-    coordinates: {} as Coordinates,
   }),
-  persist: [
-    {
-      pick: ["coordinates"],
-      storage: piniaPluginPersistedstate.localStorage(),
-    },
-  ],
 });
